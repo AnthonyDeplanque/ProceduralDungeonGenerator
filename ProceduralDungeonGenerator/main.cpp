46769fe9760eeba6c79e89f2d9f812e0650c6c1a
Anthony deplanque <anthony.deplanque3184@gmail.com>
2023-10-24 16:35:31 +0200

    no Hard Coded variables for UnrealEngine integration

--- ProceduralDungeonGenerator/main.cpp
+++ ProceduralDungeonGenerator/main.cpp
@@ -5,20 +5,26 @@
 
 int main()
 {
+	int ageToGenerate = 1;
+	int xOrigin = 0;
+	int yOrigin = 0;
+	bool DEBUG = true;
 
 	Dungeon dungeon;
 
-	dungeon.addRoom(std::make_shared<Room>(Room(0, 0, 0)));
+	dungeon.addRoom(std::make_shared<Room>(Room(xOrigin, yOrigin, 0)));
 
 	std::shared_ptr<Room> dungeonRoom = dungeon.getRoomPtrList()[0];
 
-	for (int i = 0; i <20; i++)
+	for (int i = 0; i < ageToGenerate; i++)
 	{
 		dungeon.generateRoomsByLastGeneratedRooms(i);
 	}
 
-	dungeon.printRoomList();
-
+	if (DEBUG)
+	{
+		dungeon.printRoomList();
+	}
 
 	return EXIT_SUCCESS;
 }
